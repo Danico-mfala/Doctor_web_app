5285959e21514ca6bb4fd2cb9257e42299383cc0
Danico-Mfala <mfaladan@gmail.com>
2024-02-05 21:56:21 -0800

    uploading on git

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./app.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-[20px] text-blue-600">React + Redux + Thunk</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Routers = () => {
+  return <div>Rooters</div>;
+};
+
+export default Routers;
